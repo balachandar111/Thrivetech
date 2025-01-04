ec1ff53c7539e31020629f35439b5a796a5f2af3
balachandar <balachandarsanthoshkumar111@gmail.com>
2025-01-04 17:47:37 +0530

    functions

--- thrivetechapp/views.py
+++ thrivetechapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return render(request,"home.html")
+def whoweare(request):
+    return render(request,"test.html")
+def migration(request):
+    return render(request,"migration1.html")
+def career(request):
+    return render(request,"career.html")
+def helpdesk(request):
+    return render(request,"helpdesk.html")
 
-# Create your views here.
+def customersupport(request):
+    return render(request,"customersupport.html")
+def uiux(request):
+    return render(request,"uiux.html")
+def website(request):
+    return render(request,"website.html")
+
+def forge(request):
+    return render(request,"forge.html")
